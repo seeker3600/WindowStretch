cdbb55689d92f863e180043a3bf6e90e8474a87f
seeker3600 <29515315+seeker3600@users.noreply.github.com>
2021-04-05 01:20:44 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowStretch/Src/Core/StretchUtils.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Windows.Sdk;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace WindowStretch.Core
+{
+    using static StretchMode;
+
+    public static class StretchUtils
+    {
+        public static void Stretch(IntPtr hwndIp, StretchPattern pat)
+        {
+            var hwnd = new HWND(hwndIp);
+
+            Rectangle rect;
+            bool alwaysTop = pat.AlwaysTop;
+            bool resetPosition = false;
+
+            switch (pat.Mode)
+            {
+                case FullScreen:
+                    alwaysTop = true;
+                    resetPosition = true;
+                    rect = GetAfterSizeWhenFullScreen(hwnd);
+                    break;
+
+                case MaxOnDesktop:
+                    resetPosition = true;
+                    rect = GetAfterSizeWhenMaxOnDesktop(hwnd);
+                    break;
+
+                case Manual:
+                    rect = new Rectangle(0, 0, pat.ManualWidth, pat.ManualHeight);
+                    break;
+
+                default:
+                    return;
+            }
+
+            var hwndTop = alwaysTop ? Constants.HWND_TOPMOST : Constants.HWND_NOTOPMOST;
+
+            SetWindowPos_uFlags flags = default;
+            if (!resetPosition)
+                flags |= SetWindowPos_uFlags.SWP_NOMOVE;
+
+            PInvoke.SetWindowPos(hwnd, hwndTop, rect.X, rect.Y, rect.Width, rect.Height, flags);
+        }
+
+        // ratio > 1 なら横長、ratio < 1 なら縦長
+        public static float GetWindowAspectRatio(IntPtr hwndIp)
+        {
+            if (!PInvoke.GetWindowRect(new HWND(hwndIp), out var rect))
+                throw new InvalidOperationException(nameof(PInvoke.GetWindowRect));
+
+            var res = (float)(rect.right - rect.left) / (rect.bottom - rect.top);
+            Debug.Assert(res > 0);
+
+            return res;
+        }
+
+        private static Rectangle GetAfterSizeWhenMaxOnDesktop(HWND hwnd)
+        {
+            var ratio = GetWindowAspectRatio(hwnd);
+
+            // ウィンドウ領域の寸法を計算
+            var area = Screen.FromHandle(hwnd).WorkingArea;
+            var (width, height) = GetInnerMaxSize(area.Width, area.Height, ratio);
+
+            // ウィンドウ位置を計算
+            var left = area.X + (area.Width - width) / 2;
+            var top = area.Y + (area.Height - height) / 2;
+
+            // ウィンドウ領域を返却
+            return new Rectangle(left, top, width, height);
+        }
+
+        private static Rectangle GetBorder(HWND hwnd)
+        {
+            // クライアント領域の寸法を取得
+            if (!PInvoke.GetClientRect(hwnd, out var clientRect))
+                throw new InvalidOperationException(nameof(PInvoke.GetClientRect));
+
+            // クライアント領域の位置を取得
+            var point = new POINT() { x = 0, y = 0 };
+            if (!PInvoke.ClientToScreen(hwnd, ref point))
+                throw new InvalidOperationException(nameof(PInvoke.ClientToScreen));
+
+            // クライアント領域を計算
+            clientRect.left += point.x;
+            clientRect.right += point.x;
+            clientRect.top += point.y;
+            clientRect.bottom += point.y;
+
+            // ウィンドウ領域を取得
+            if (!PInvoke.GetWindowRect(hwnd, out var windowRect))
+                throw new InvalidOperationException(nameof(PInvoke.GetWindowRect));
+
+            return Rectangle.FromLTRB(
+                windowRect.left - clientRect.left,
+                windowRect.top - clientRect.top,
+                windowRect.right - clientRect.right,
+                windowRect.bottom - clientRect.bottom
+            );
+        }
+
+        private static Rectangle GetAfterSizeWhenFullScreen(HWND hwnd)
+        {
+            var ratio = GetWindowAspectRatio(hwnd);
+
+            // ウィンドウ領域の寸法を計算
+            var area = Screen.FromHandle(hwnd).Bounds;
+            var border = GetBorder(hwnd);
+            var (width, height) = GetInnerMaxSize(area.Width + border.Width, area.Height + border.Height, ratio);
+
+            // ウィンドウ位置を計算
+            var left = area.X + (area.Width + border.Width - width) / 2 + border.Left;
+            var top = area.Y + (area.Height + border.Height - height) / 2 + border.Top;
+
+            // ウィンドウ領域を返却
+            return new Rectangle(left, top, width, height);
+        }
+
+        private static (int width, int height) GetInnerMaxSize(int width, int height, float ratio)
+        {
+            if (width / ratio <= height)
+                return (width, (int)(width / ratio));
+            else
+                return ((int)(height * ratio), height);
+        }
+
+    }
+}
---
+++ WindowStretch/Src/Main/MainForm.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace WindowStretch.Main
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.statusLbl = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.modeBoxT = new System.Windows.Forms.ComboBox();
+            this.alwaysTopChkT = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.modeBoxW = new System.Windows.Forms.ComboBox();
+            this.alwaysTopChkW = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.leftNumW = new System.Windows.Forms.NumericUpDown();
+            this.watchTimer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftNumW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.updateBtn);
+            this.flowLayoutPanel1.Controls.Add(this.statusLbl);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(5);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(658, 39);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(8, 8);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(75, 23);
+            this.updateBtn.TabIndex = 3;
+            this.updateBtn.Text = "再適用";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.AutoSize = true;
+            this.statusLbl.Location = new System.Drawing.Point(89, 5);
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(51, 15);
+            this.statusLbl.TabIndex = 2;
+            this.statusLbl.Text = "ステータス";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 39);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 111F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(658, 111);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.AutoSize = true;
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.modeBoxT, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.alwaysTopChkT, 0, 2);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(332, 3);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 3;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(323, 101);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(317, 47);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "縦長のとき";
+            // 
+            // modeBoxT
+            // 
+            this.modeBoxT.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modeBoxT.FormattingEnabled = true;
+            this.modeBoxT.Location = new System.Drawing.Point(3, 50);
+            this.modeBoxT.Name = "modeBoxT";
+            this.modeBoxT.Size = new System.Drawing.Size(192, 23);
+            this.modeBoxT.TabIndex = 1;
+            this.modeBoxT.SelectedIndexChanged += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // alwaysTopChkT
+            // 
+            this.alwaysTopChkT.AutoSize = true;
+            this.alwaysTopChkT.Location = new System.Drawing.Point(3, 79);
+            this.alwaysTopChkT.Name = "alwaysTopChkT";
+            this.alwaysTopChkT.Size = new System.Drawing.Size(62, 19);
+            this.alwaysTopChkT.TabIndex = 2;
+            this.alwaysTopChkT.Text = "最前面";
+            this.alwaysTopChkT.UseVisualStyleBackColor = true;
+            this.alwaysTopChkT.CheckedChanged += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.modeBoxW, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.alwaysTopChkW, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(323, 101);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "横長のとき";
+            // 
+            // modeBoxW
+            // 
+            this.modeBoxW.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modeBoxW.FormattingEnabled = true;
+            this.modeBoxW.Location = new System.Drawing.Point(3, 50);
+            this.modeBoxW.Name = "modeBoxW";
+            this.modeBoxW.Size = new System.Drawing.Size(192, 23);
+            this.modeBoxW.TabIndex = 1;
+            this.modeBoxW.SelectedIndexChanged += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // alwaysTopChkW
+            // 
+            this.alwaysTopChkW.AutoSize = true;
+            this.alwaysTopChkW.Location = new System.Drawing.Point(3, 79);
+            this.alwaysTopChkW.Name = "alwaysTopChkW";
+            this.alwaysTopChkW.Size = new System.Drawing.Size(62, 19);
+            this.alwaysTopChkW.TabIndex = 2;
+            this.alwaysTopChkW.Text = "最前面";
+            this.alwaysTopChkW.UseVisualStyleBackColor = true;
+            this.alwaysTopChkW.CheckedChanged += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "上";
+            // 
+            // leftNumW
+            // 
+            this.leftNumW.Location = new System.Drawing.Point(157, 3);
+            this.leftNumW.Name = "leftNumW";
+            this.leftNumW.Size = new System.Drawing.Size(120, 23);
+            this.leftNumW.TabIndex = 4;
+            // 
+            // watchTimer
+            // 
+            this.watchTimer.Enabled = true;
+            this.watchTimer.Interval = 1000;
+            this.watchTimer.Tick += new System.EventHandler(this.watchTimer_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "WindowStretch";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 150);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WindowStretch";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftNumW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox alwaysTopChkW;
+        private System.Windows.Forms.ComboBox modeBoxW;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown leftNumW;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox modeBoxT;
+        private System.Windows.Forms.CheckBox alwaysTopChkT;
+        private System.Windows.Forms.Label statusLbl;
+        private System.Windows.Forms.Timer watchTimer;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
