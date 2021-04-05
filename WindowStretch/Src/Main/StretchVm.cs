2ce850c65f342fe04f05f1a0708a35a9edabbbbb
hiro <29515315+seeker3600@users.noreply.github.com>
2021-04-05 19:36:35 +0900

    最小化に関する処理を修正。

--- WindowStretch/Src/Main/StretchVm.cs
+++ WindowStretch/Src/Main/StretchVm.cs
@@ -5,11 +5,15 @@ using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 using WindowStretch.Core;
+using WindowStretch.Properties;
 
 namespace WindowStretch.Main
 {
+    using static FormWindowState;
 
     public class StretchVm
     {
@@ -20,6 +24,25 @@ namespace WindowStretch.Main
         public StretchPatternVm Tall { get; } = new();
 
         public ReactivePropertySlim<string> StatusMsg { get; } = new();
+
+        public ReactivePropertySlim<FormWindowState> WindowState { get; } = new();
+
+        public StretchVm(MainForm form)
+        {
+            WindowState
+                .Where(st => st == Minimized)
+                .Subscribe(_ => form.Visible = false);
+
+            WindowState
+                .Pairwise()
+                .Where(st => st.OldItem != Normal && st.NewItem == Normal)
+                .Subscribe(_ =>
+                {
+                    form.Visible = true;
+                    form.WindowState = Normal;
+                    form.Activate();
+                });
+        }
 
         private float? BeforeRatio = null;
 
@@ -67,19 +90,24 @@ namespace WindowStretch.Main
 
         public void Load()
         {
-            Wide.Mode.Value = (StretchMode)Properties.Settings.Default.WideMode;
-            Wide.AlwaysTop.Value = Properties.Settings.Default.WideAlwaysTop;
-            Tall.Mode.Value = (StretchMode)Properties.Settings.Default.TallMode;
-            Tall.AlwaysTop.Value = Properties.Settings.Default.TallAlwaysTop;
+            Wide.Mode.Value = (StretchMode)Settings.Default.WideMode;
+            Wide.AlwaysTop.Value = Settings.Default.WideAlwaysTop;
+            Tall.Mode.Value = (StretchMode)Settings.Default.TallMode;
+            Tall.AlwaysTop.Value = Settings.Default.TallAlwaysTop;
+
+            WindowState.Value = (FormWindowState)Settings.Default.WindowState;
         }
 
         public void Save()
         {
-            Properties.Settings.Default.WideMode = (int)Wide.Mode.Value;
-            Properties.Settings.Default.WideAlwaysTop = Wide.AlwaysTop.Value;
-            Properties.Settings.Default.TallMode = (int)Tall.Mode.Value;
-            Properties.Settings.Default.TallAlwaysTop = Tall.AlwaysTop.Value;
-            Properties.Settings.Default.Save();
+            Settings.Default.WideMode = (int)Wide.Mode.Value;
+            Settings.Default.WideAlwaysTop = Wide.AlwaysTop.Value;
+            Settings.Default.TallMode = (int)Tall.Mode.Value;
+            Settings.Default.TallAlwaysTop = Tall.AlwaysTop.Value;
+
+            Settings.Default.WindowState = (int)WindowState.Value;
+
+            Settings.Default.Save();
         }
     }
 
