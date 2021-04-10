60e13c1d39bb57b3abe1155b4589b5660177c21f
seeker3600 <29515315+seeker3600@users.noreply.github.com>
2021-04-10 16:54:07 +0900

    アプリの起動機能を追加。

---
+++ WindowStretch/Src/Main/StartVm.cs
@@ -0,0 +1,55 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowStretch.Properties;
+
+namespace WindowStretch.Src.Main
+{
+    public class StartVm
+    {
+        public ReactivePropertySlim<string> Uri { get; } = new();
+
+        public ReactivePropertySlim<bool> StartWithMe { get; } = new();
+
+        public ReactivePropertySlim<string> Status { get; } = new();
+
+        public void Load()
+        {
+            Uri.Value = Settings.Default.StartAppUri;
+            StartWithMe.Value = Settings.Default.StartWithMe;
+
+            if (StartWithMe.Value) Start();
+        }
+
+        public void Save()
+        {
+            Settings.Default.StartAppUri = Uri.Value;
+            Settings.Default.StartWithMe = StartWithMe.Value;
+
+            Settings.Default.Save();
+        }
+
+        public void Start()
+        {
+            try
+            {
+                var info = new ProcessStartInfo(Uri.Value)
+                {
+                    UseShellExecute = true
+                };
+
+                Process.Start(info);
+
+                Status.Value = "アプリを起動しました。";
+            }
+            catch (Exception)
+            {
+                Status.Value = "アプリの起動に失敗しました。タイプミス、管理者権限などを確認してください。";
+            }
+        }
+    }
+}
